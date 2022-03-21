83cd0401297632d8606fd3657bbd5e544415e0be
huyle135 <92618585+huyle135@users.noreply.github.com>
2022-03-21 02:35:32 -0700

    Delete bai_2 directory

--- bai_2/bai_2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$o= new PDO('mysql:host=localhost;dbname=book','root','');
-$o->query('set names utf8');
-$sql="select * from book";
-$stm=$o->query($sql);
-$data=$stm->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($data);
